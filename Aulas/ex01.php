bea297d266a0f4fb526db098b86ce0625862b16b
Francisco Macedo <macedolb53@gmail.com>
2024-03-30 12:45:04 +0100

    Criei o meu exemplo com PHP

---
+++ Aulas/ex01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VAmos fazer o nosso Primeiro Exemplo usando Versionamento com git e github</title>
+</head>
+<body>
+    <main>
+        <?php
+            echo"Hello Word!" 
+        ?>
+    </main>
+    
+</body>
+</html>
