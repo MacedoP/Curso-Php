4979bf94f0d6a63234228ca802f11483b3cf3069
Francisco Macedo <macedolb53@gmail.com>
2024-04-09 01:27:34 +0200

    p

--- Aulas/ex01.php
+++ Aulas/ex01.php
@@ -4,17 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VAmos fazer o nosso Primeiro Exemplo usando Versionamento com git e github</title>
+    <link rel="stylesheet" href="./stilos/stilo-geral.css">
 </head>
 <body>
     <main>
         <?php
-            $nome = "Macedo";
-            $idade = 23;
+           
+           
+          
 
-            echo"O seu nome é: $nome e tens $idade de idade!";
-            print"<br>";
-            echo"Hello Word!" ;
-            echo"Bom dia";
+            function notaFinal($s){
+                $notas = func_get_args();
+                $qn = func_num_args();
+                $media =0;
+                for($i =0; $i < $notas;$i++){
+                    $media += $notas[$i];
+                }
+                return $media;
+                
+
+            }
+            $aluno = array(4.3,5.7,2.1,6.7);
+            echo notaFinal($aluno) ;
+
         ?>
     </main>
     
